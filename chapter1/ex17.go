bb6377f304a6d448d7cf2342cf079b9d14cc8f52
Alexander Moses <moijes12@gmail.com>
2020-04-27 17:56:40 +0530

    Added solution for ex1.7, ex1.8 and ex1.9
    Updated the fetch program to read use io.Copy instead of ioutil.ReadAll, Updated the URL with http:// wherever it is missing and printed the HTTP Status Code

---
+++ chapter1/ex17.go
@@ -0,0 +1,33 @@
+// Function ro read from a URL
+// And write to Stdout
+// This also implements the solutions
+// to exercise 1.8 and exercise 1.9
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+    for _, url := range os.Args[1:] {
+    	if strings.HasPrefix(url, "http://") == false {
+    		url = "http://" + url
+    	}
+    	resp, err := http.Get(url)
+    	if err != nil {
+    		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+    		os.Exit(1)
+    	}
+    	fmt.Printf("HTTP Status Code of response : %v\n", resp.Status)
+    	io.Copy(os.Stdout, resp.Body)
+    	resp.Body.Close()
+    	if err != nil {
+    		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+    		os.Exit(1)
+    	}
+    }	
+}
